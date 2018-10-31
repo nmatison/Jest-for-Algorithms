e0abaa92b569a02cb859f81fcd7aa02bc781d9c4
Nick <nmatison@gmail.com>
2018-10-30 20:27:31 -0700

    Create destroy tests for edge and destroy method to test it out

--- graphs/graph.js
+++ graphs/graph.js
@@ -4,8 +4,8 @@ class Vertex {
     this.value = value;
     this.in_edges = [];
     this.out_edges = [];
-  }
-}
+  };
+};
 
 class Edge {
 
@@ -14,13 +14,34 @@ class Edge {
     this.from_vertex = from_vertex;
     this.to_vertex = to_vertex;
     this.handleEdges()
-  }
+  };
 
   handleEdges() {
     this.from_vertex.out_edges.push(this);
     this.to_vertex.in_edges.push(this);
-  }
-}
+  };
+
+  destroy() {
+    let newOutEdges = [];
+    let newInEdges = [];
+    for(let i = 0; i < this.from_vertex.out_edges.length; i++) {
+      if (this.from_vertex.out_edges[i] !== this ) {
+        newOutEdges.push(this.from_vertex.out_edges[i]);
+      }
+    };
+
+    for(let i = 0; i < this.to_vertex.in_edges.length; i++) {
+      if (this.to_vertex.in_edges[i] !== this ) {
+        newInEdges.push(this.to_vertex.in_edges[i]);
+      }
+    };
+
+    this.from_vertex.out_edges = newOutEdges;
+    this.to_vertex.in_edges = newInEdges;
+    this.from_vertex = null;
+    this.to_vertex = null;
+  };
+};
   
 
 module.exports = {
--- graphs/graph.test.js
+++ graphs/graph.test.js
@@ -27,15 +27,15 @@ describe('Vertex', () => {
 });
 
 describe('Edge', () => {
+  let from_vertex = new Vertex(null);
+  let to_vertex = new Vertex(null);
+  let testEdge;
+
+  beforeAll(() => {
+    testEdge = new Edge(from_vertex, to_vertex)
+  });
+
   describe('initialize', () => {
-    let from_vertex = new Vertex(null);
-    let to_vertex = new Vertex(null);
-    let testEdge;
-
-    beforeAll(() => {
-      testEdge = new Edge(from_vertex, to_vertex)
-    });
-
     test("it stores a from_vertex", () => {
       expect(testEdge.from_vertex).toBe(from_vertex)
     });
@@ -49,12 +49,27 @@ describe('Edge', () => {
     });
 
     test(`it adds itself to the to_vertex's in edges`, () => {
-      expect(to_vertex.in_edges[to_vertex.in_edges.length - 1]).toBe(testEdge)
+      expect(to_vertex.in_edges).toContain(testEdge)
     });
 
     test(`it adds itself to the from_vertex's out edges`, () => {
-      expect(from_vertex.out_edges[from_vertex.out_edges.length - 1]).toBe(testEdge)
+      expect(from_vertex.out_edges).toContain(testEdge)
+    });
+  });
+  
+  describe('destroy', () => {
+    beforeAll(() => {
+      testEdge.destroy()
     });
 
+    test(`it deletes itself from its to_vertex's and from_vertex's edges`, () => {
+      expect(to_vertex.in_edges).not.toContain(testEdge);
+      expect(from_vertex.out_edges).not.toContain(testEdge);
+    });
+
+    test(`it sets its verticies to nil`, () => {
+      expect(testEdge.from_vertex).toBeNull();
+      expect(testEdge.to_vertex).toBeNull();
+    });
   });
 });
