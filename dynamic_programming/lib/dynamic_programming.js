a63f00b483a7ede3fe668b338e47e62239f081b5
Nick <nmatison@gmail.com>
2018-11-03 17:33:50 -0700

    Add specs for Frog Hops top down

--- dynamic_programming/lib/dynamic_programming.js
+++ dynamic_programming/lib/dynamic_programming.js
@@ -1,5 +1,4 @@
 class DynamicProgramming {
-
   constructor() {
     this.blairCache = {
       1: 1,
@@ -14,6 +13,39 @@ class DynamicProgramming {
     return this.blairCache[n];
   };
 
+  frogHopsBottomUp(n) {
+    return this.frogCacheBuilder(n)
+  };
+
+  frogCacheBuilder(n) {
+    let cache = {
+      1: [[1]],
+      2: [[1, 1], [2]],
+      3: [[1, 1, 1], [1, 2], [2, 1], [3]]
+    };
+    
+    if (cache[n]) {
+      return cache[n];
+    }
+
+    for(let i = 4; i <= n; i++) {
+      cache[i] = [];
+      cache[i] = cache[i].concat(this.frogHopsBottomHelper(cache[i - 3], 3));
+      cache[i] = cache[i].concat(this.frogHopsBottomHelper(cache[i - 2], 2));
+      cache[i] = cache[i].concat(this.frogHopsBottomHelper(cache[i - 1], 1));
+    };
+    return cache[n].sort();
+  };
+
+  frogHopsBottomHelper(arr, n) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      let temp = arr[i].slice(0)
+      temp.push(n);
+      result.push(temp);
+    };
+    return result;
+  };
 }
 
 module.exports = DynamicProgramming;
---
+++ dynamic_programming/tests/frog_hops.test.js
@@ -0,0 +1,31 @@
+const dynamicProgramming = require('../lib/dynamic_programming');
+
+describe('Frog Hops Bottom Up', () => {
+  let dp = new dynamicProgramming();
+
+  describe('Frog Hops Bottom Up', () => {
+    test('it handles a base case', () => {
+      expect(dp.frogHopsBottomUp(1)).toEqual([[1]]);
+      expect(dp.frogHopsBottomUp(2)).toEqual([[1, 1], [2]]);
+    });
+
+    test('it handles recursive cases', () => {
+      expect(dp.frogHopsBottomUp(4)).toEqual([[1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2], [1, 3], [3, 1]].sort());
+      expect(dp.frogHopsBottomUp(10)).toHaveLength(274);
+      expect(dp.frogHopsBottomUp(10)).toContainEqual([1, 3, 3, 3]);
+    });
+  });
+
+  describe('Frog Hops Top Down', () => {
+    test('it handles a base case', () => {
+      expect(dp.frogHopsTopDown(1)).toEqual([1])
+      expect(dp.frogHopsTopDown(2)).toEqual([[1, 1], [2]]);
+    });
+
+    test('it handles recursive cases', () => {
+      expect(dp.frogHopsTopDown(4)).toEqual([[1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2], [1, 3], [3, 1]].sort());
+      expect(dp.frogHopsTopDown(10)).toHaveLength(274);
+      expect(dp.frogHopsTopDown(10)).toContainEqual([1, 3, 3, 3]);
+    });
+  });
+});
