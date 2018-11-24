04d686f6a8b48df80f0ff9b9ce9db977209b0f98
Nick <nmatison@gmail.com>
2018-11-24 10:52:53 -0800

    Clean up Khan's algorithm without destroying edges solution

--- solutions/graphs/topological_sort.js
+++ solutions/graphs/topological_sort.js
@@ -1,30 +1,46 @@
 const Vertex = require("../lib/graph").Vertex;
 const Edge = require("../lib/graph").Edge;
 
+// Kahn's Algorithm with destroying the edges.
+const topologicalSort = function(vertices) {
+  let result = [];
+  let queue = [];
+  for(let i = 0; i < vertices.length; i++) {
+    if (vertices[i].in_edges.length === 0) queue.push(vertices[i]);
+  };
+
+
+}
+
+//  Kahn's Algorithm without destroying the edges.
 const topologicalSort = function(vertices) {
   let in_edge_count = {};
   let result = [];
-  for(let i = 0; i < vertices.length; i++) {
-    in_edge_count[vertices[i].value] = vertices[i].in_edges.length;
-  };
-
   let queue = [];
-  for(let i = 0; i < vertices.length; i++) {
+
+  for (let i = 0; i < vertices.length; i++) {
     let vertex = vertices[i];
+
+    in_edge_count[vertex.value] = vertex.in_edges.length;
     if (in_edge_count[vertex.value] == 0) queue.push(vertex);
-  };
+  }
 
   while (queue.length != 0) {
     let popped = queue.shift();
-    popped.out_edges.forEach((edge) => {
+
+    popped.out_edges.forEach(edge => {
       let destination = edge.to_vertex;
       in_edge_count[destination.value] -= 1;
       if (in_edge_count[destination.value] === 0) queue.push(destination);
     });
+    
     result.push(popped);
-  };
+  }
 
   return result.length === vertices.length ? result : [];
 };
+
+module.exports = topologicalSort; 
+
 
 module.exports = topologicalSort; 
